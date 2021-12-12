8dedb860d6bfa22fa2694503add2b5db3d2b93d5
Nikhil Choudhary <nc1336282@gmail.com>
2021-12-12 19:14:16 +0530

    Added build script and readme file

---
+++ src/index.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { screen, render, cleanup, fireEvent } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  test('should simply pass', () => {
+    console.log('simple passing test');
+  });
+});
